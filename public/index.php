396ec25575a3ff4ac1cf8f467f69b1ff2ffdca72
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-03-24 15:02:24 +0100

    Correct reponse to send response

--- public/index.php
+++ public/index.php
@@ -15,16 +15,15 @@ $request = Request::createFromGlobals();
 $response = new Response();
 $routes = require __DIR__ . '/../config/routes.php';
 
-
 foreach ($routes as $route) {
     if ($request->getMethod() === $route[0] && $request->getRequestUri() === $route[1]) {
         $content = call_user_func($route[2]);
         $response->setContent($content);
     }
-};
+}
 
 if (!$response->getContent()) {
     $response = new Response('404 - Not Found', 404);
 }
 
-echo $response;
+$response->send();
