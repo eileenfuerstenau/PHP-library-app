2e911dfa5eab979ac3f171b53919ce15803419ed
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-03-24 14:58:23 +0100

    Add Request/ Response Object

--- public/index.php
+++ public/index.php
@@ -1,29 +1,30 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-error_reporting(E_ERROR);
-ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-$method = $_SERVER['REQUEST_METHOD'];
-$uri = $_SERVER['REQUEST_URI'];
+$request = Request::createFromGlobals();
+$response = new Response();
 $routes = require __DIR__ . '/../config/routes.php';
-$response = '';
 
 
 foreach ($routes as $route) {
-    if ($method === $route[0] && $uri === $route[1]) {
-        $response = call_user_func($route[2]);
+    if ($request->getMethod() === $route[0] && $request->getRequestUri() === $route[1]) {
+        $content = call_user_func($route[2]);
+        $response->setContent($content);
     }
 };
 
-if (!$response) {
-    http_response_code(404);
-    $response = '404 - Not Found';
-};
+if (!$response->getContent()) {
+    $response = new Response('404 - Not Found', 404);
+}
 
 echo $response;
